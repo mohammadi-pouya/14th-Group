764dbdd129503d492dd8cb7638261c65bd2335c0
Pouya Mohammadi <p.mohammady72@gmail.com>
2022-11-20 10:41:08 +0330

    refactor

--- mohamadi-pouya-Python/6. Week 6 - Fibonacci.py
+++ mohamadi-pouya-Python/6. Week 6 - Fibonacci.py
@@ -11,6 +11,19 @@ def fib(n):
             previous, current = current, previous + current
             print(current)
 
+# Solution2
+def fibo(n):
+    f0 = 0
+    f1 = 1
+    for i in range(n + 1):
+        next_num = f0 + f1
+        yield f0
+        f0 = f1
+        f1 = next_num
+    
 
 if __name__ == '__main__':
-    fib(2)
+    fib(20)
+    
+    for item in fibo(20):
+        print(item)
